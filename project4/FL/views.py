3bc19005ae5f9ffdc45acf07ec4a489f8b42db54
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-08 09:13:34 +0900

    setting and all
    xml json 으로 파싱처리 테스트

--- project4/FL/views.py
+++ project4/FL/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+import requests
+import xmltodict as xmltodict
+import json
+
+
+@api_view(['GET'])
+def ursafe(request):
+    if request.method == "GET":
+        apiUrl = 'https://www.maniadb.com/api/search/metallica/?sr=artist&display=10&key=example&v=0.5'
+
+        req = requests.get(apiUrl)
+
+        xpars = xmltodict.parse(req.text)
+
+        jsonDump = json.dumps(xpars)
+
+        jsonBody = json.loads(jsonDump)
+        return Response(jsonBody)
